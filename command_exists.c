46421439d758f5ad14cc60e5aae703fa3d44fc29
Jeremy Cardona <jeremy.cardona@hotmail.com>
2025-01-13 12:25:03 -0400

    first commit

---
+++ command_exists.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * command_exists - Checks if a command exists at the given path
+ * @cmd: Pointer to the command
+ *
+ * Return: 1 if the file exists and 0 if it doesn't
+ */
+
+int command_exists(char *cmd)
+{
+	struct stat buffer;
+
+	return (stat(cmd, &buffer) == 0);
+}
---
+++ main.h
@@ -0,0 +1,39 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <ctype.h>
+
+extern char **environ;
+
+#define MAX_COMMAND_LENGTH 100
+/*
+ * interactive mode definitions here
+ **/
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define MAX_ARGS 64
+#define MAX_PATH_LENGTH 1024
+#define MAX_INPUT_SIZE 1024
+
+extern char **environ;
+/* Function declarations */
+int command_exists(char *cmd);
+int find_command_in_path(char *cmd, char *fullpath);
+void handle_cat(char *filename);
+int is_path(char *cmd);
+int handle_cd(char *path);
+int execute_command(char *cmd, char *arg, char *fullpath);
+void handle_env(char *cmd);
+void handle_exit(char *cmd);
+
+#endif /* MAIN.H */
